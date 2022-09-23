849a722e46b5ca64f19c71871ebbf0fe3d00f086
giuratofabrizio <giurato.fabrizio@iisgalvanimi.edu.it>
2022-09-23 08:44:39 +0000

    fineprovaangular

---
+++ angular-hello-world/src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'; //Molto simile alle import in Java
+
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.component.html',
+  styleUrls: ['./hello-world.component.css']
+})
+export class HelloWorldComponent implements OnInit {
+  constructor() { }
+  ngOnInit() {
+  }
+}
