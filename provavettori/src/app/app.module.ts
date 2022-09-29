56f9bb877906202c39baed87ddd65d15bea343ed
giuratofabrizio <giurato.fabrizio@iisgalvanimi.edu.it>
2022-09-29 10:58:51 +0000

    provavettori

---
+++ provavettori/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserItemComponent,
+    HelloWorldComponent,
+    UserListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
